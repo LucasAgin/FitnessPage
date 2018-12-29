06eadc4765c3a16db2d1474d3c5122976fd4dd47
Lucas <lucas.agin@live.com>
2018-12-29 15:59:49 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Headerss extends Component {
+  render() {
+    return <div> Welcome to my site in {this.props.month}</div>;
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Headerss month="dec"></Headerss>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
